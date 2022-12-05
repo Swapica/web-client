0a4f8af53c132e8a6077f4f00e858c8309ccf821
electr1xx <aleksandrplotnikov1997@gmail.com>
2022-12-05 20:20:32 +0200

    add web3

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,5 @@
 export * from './common.types'
+export * from './provider.types'
+export * from './ethereum.types'
+export * from './solana.types'
+export * from './contracts'
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ import checker from 'vite-plugin-checker'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
-import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 import * as fs from 'fs'
@@ -17,11 +16,9 @@ const resolveApp = (relative: string) => path.resolve(appDirectory, relative)
 const root = path.resolve(__dirname, resolveApp('src'))
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
-  const isProduction = env.VITE_APP_ENVIRONMENT === 'production'
-  const isDevelopment = env.VITE_APP_ENVIRONMENT === 'development'
   const isAnalyze = env.VITE_APP_ENVIRONMENT === 'analyze'
 
   return {
@@ -70,18 +67,6 @@ export default defineConfig(({ command, mode }) => {
         '@': `${root}/`,
         '@config': `${root}/config.ts`,
         '@static': `${root}/../static`,
-      },
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: 'globalThis',
-        },
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-          }),
-        ],
       },
     },
     test: {
