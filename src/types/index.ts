9a552b685eb1bb3366e7c9df3c8746d45069fae7
electr1xx <aleksandrplotnikov1997@gmail.com>
2022-12-26 20:39:50 +0200

    remove unused providers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,6 @@
 export * from './common.types'
 export * from './provider.types'
 export * from './ethereum.types'
-export * from './solana.types'
 export * from './contracts'
 export * from './api.types'
 export * from './order.types'
